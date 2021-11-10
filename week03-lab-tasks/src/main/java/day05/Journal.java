6f14d1c2aa65f3a850e83eb478e356aa7fe35fd1
Emeriscus <kimre88@gmail.com>
2021-11-10 16:18:54 +0100

    day05.Journal final v1.1

--- week03-lab-tasks/src/main/java/day05/Journal.java
+++ week03-lab-tasks/src/main/java/day05/Journal.java
@@ -8,7 +8,7 @@ public class Journal {
     private List<String> names = new ArrayList<>();
 
     public boolean addStudent(String studentName) {
-        if (studentName.contains(" ")) {
+        if (studentName.contains(" ")) { // + trim : studentName.trim().contains(" ")
             names.add(studentName);
             System.out.println("A név megfelelő");
             return true;
@@ -19,6 +19,7 @@ public class Journal {
 
     public List<String> getNames() {
         return names;
+//        return new ArrayList<>(names);        // elegánsabb getter megoldás, bár memória pazarlóbb...
     }
 
     @Override
